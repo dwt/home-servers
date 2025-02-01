60c8352cffb51a3ecec9a34e18f1d3d5b8786b89
Martin Häcker <mhaecker@mac.com>
2025-02-01 15:51:21 +0100

    Switch to different way of injecting lix
    Now only via the lix-module, not by explicitly referencing lix.
    This works now, because I have the linux builder, and do not need to build in the container anymore.

--- flake.nix
+++ flake.nix
@@ -2,20 +2,17 @@
   description = "Häckers NixOS Homeserver configurations";
 
   inputs = {
+    # raspi kernel doesn't build on nixos-24.11 because of
+    # https://github.com/NixOS/nixpkgs/pull/377972
+    # which is not yet backported into 24.11. That should happen about next week though
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    # raspberry pi hardware description
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-
-    # lix - but doesn't work as of yet
-    # See https://wiki.lix.systems/books/lix-contributors/page/lix-beta-guide
-    # See https://git.lix.systems/lix-project/lix/issues/452
-    lix.url = "https://git.lix.systems/lix-project/lix/archive/main.tar.gz";
-    lix.flake = false;
-    # lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
-    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    # lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.92.0.tar.gz";
     lix-module.inputs.nixpkgs.follows = "nixpkgs";
-    lix-module.inputs.lix.follows = "lix";
-
     # secrets management
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
@@ -59,8 +56,8 @@
       };
     }
     // flake-utils.lib.simpleFlake {
+      name = "pi";
       inherit self nixpkgs;
-      name = "fnord";
       shell = ./shell.nix;
     };
   # See https://git.berlin.ccc.de/cccb/ringbahn/src/branch/main/pkgs/reencrypt-secrets.nix
