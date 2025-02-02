47f810bba327af7d7a8e101a3e99137e389dc76f
Martin Häcker <mhaecker@mac.com>
2025-02-02 16:47:28 +0100

    Document how the ssh key plays into sops

--- configuration.nix
+++ configuration.nix
@@ -50,16 +50,25 @@ in
   # TODO samba setup
   # see https://nixos.wiki/wiki/Samba
 
+  # define secret
   sops.secrets."wireless.secrets" = { };
   networking.wireless.secretsFile = config.sops.secrets."wireless.secrets".path;
   networking.wireless.enable = true;
   networking.wireless.networks = {
+    # SSID can come from secret too, perhaps marked as *_unencrypted
+    # not easily, as there is no secret access with sops in the evaluation of this file
     "Wo bleibt die Devolution?".pskRaw = "ext:home_psk";
   };
+  # allow usage of `wpa_cli`
+  # networking.wireless.userControlled.enable = true;
 
+  # the host key is the root of all trust in sops, so I need to write it from sops
+  # That however is not easy, as it needs the host-key to decrypt itself.
+  # Later on I plan to bake this into the sd-image - until then it's a manual process with
+  # `bin/ssh-deploy-host-key`
   services.openssh = {
     enable = true;
-    # use keys only.
+    # use keys auth only
     settings.PasswordAuthentication = false;
     settings.KbdInteractiveAuthentication = false;
   };
