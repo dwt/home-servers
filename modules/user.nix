17fda3055dc68b3ccf9987fc91fd78e75438655d
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-03-09 22:10:19 +0100

    Add ipad ssh key to user

--- modules/user.nix
+++ modules/user.nix
@@ -8,6 +8,7 @@
       hashedPasswordFile = config.sops.secrets.dwt_password_hash.path;
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJpixTba/bWGGVDM45r98YLID0t2R75U/7jL1Jblgatd dwt@seneca.local"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEgnOPFv54EK6/fxb49JxiNaD5C5qBdKs2dCANFvyxTC Terminus@iPad"
       ];
       extraGroups = [
         "wheel" # Enable ‘sudo’ for the user.
