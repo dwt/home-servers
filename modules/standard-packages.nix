f30617260b58e9d0886af95d76416cc2f43644b3
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-06-30 13:15:22 +0200

    Deploy more standard packages

--- modules/standard-packages.nix
+++ modules/standard-packages.nix
@@ -1,13 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, pkgs-unstable, ... }:
 {
   environment.systemPackages = with pkgs; [
     # nix debugging
-    nix-tree
+    pkgs-unstable.nix-tree
 
     # my dependencies
     rmate-sh
     glances
     btop
+    ncdu
+    jq
+    yq-go
   ];
-
 }
