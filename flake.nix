33157729bf7d6609ea8ee256e57d27795300e6f2
‮rekcäH nitraM‮ <spamfaenger@gmx.de>
2025-06-30 13:16:40 +0200

    Update to lix with security fix

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     # raspberry pi hardware description
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     # lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/main.tar.gz";
-    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.0.tar.gz";
+    lix-module.url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.2-1.tar.gz";
     lix-module.inputs.nixpkgs.follows = "nixpkgs";
     # secrets management
     sops-nix.url = "github:Mic92/sops-nix";
