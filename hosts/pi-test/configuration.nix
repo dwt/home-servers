4d2148db481987678573fe78ca30e49d49162b82
Martin Häcker <spamfaenger@gmx.de>
2025-12-10 10:59:18 +0100

    Disable serial access for now
    Enabling this again is for later, when I have finished my experiments
    with file sharing

--- hosts/pi-test/configuration.nix
+++ hosts/pi-test/configuration.nix
@@ -1,7 +1,8 @@
 {
   networking.hostName = "pi-test";
 
-  my.raspberry-pi.usbc-serial.enable = true;
+  # kills booting on 25.05 for me -> Explore on pi-test first
+  my.raspberry-pi.usbc-serial.enable = false;
 
   # Just a marker what the first installed version was
   # Ensures database files are kept backwards compatible with data from this version
