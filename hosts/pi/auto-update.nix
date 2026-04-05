ee5aef13896fa6e3b86f71af40f3a70f7febd183
Martin Häcker <spamfaenger@gmx.de>
2026-04-05 21:05:32 +0200

    Disable update flags as they do not work without a checkout

--- hosts/pi/auto-update.nix
+++ hosts/pi/auto-update.nix
@@ -4,8 +4,8 @@
     # Syntax: "git+https://server.com/user/repo.git#hostname"
     flake = "git+https://github.com/dwt/home-servers.git#pi";
     flags = [
-      "--update-input"
-      "nixpkgs" # update only nixpkgs
+      # these don't work when using flake reference
+      # "--update-input" "nixpkgs" # update only nixpkgs
       # "--commit-lock-file" # don't write back updates for now
     ];
     randomizedDelaySec = "45m"; # prevent server overload at 2`o clock
