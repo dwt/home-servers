c0c36b231f5a46ea8d42ecac057da433c8f72df7
Martin Häcker <spamfaenger@gmx.de>
2026-05-05 19:41:10 +0200

    Add ccc wireless to enable experimentation at ccc

--- hosts/pi-test/configuration.nix
+++ hosts/pi-test/configuration.nix
@@ -2,7 +2,7 @@
   networking.hostName = "pi-test";
 
   # kills booting on 25.05 for me -> Explore on pi-test first
-  my.raspberry-pi.usbc-serial.enable = false;
+  my.raspberry-pi.usbc-serial.enable = true;
 
   # Just a marker what the first installed version was
   # Ensures database files are kept backwards compatible with data from this version
